51d1e5e36889cf531c73e258e296ea2fb4ff24d1
wuyubin <340145294@qq.com>
2018-09-25 23:56:46 +0800

    只是把index.html和login.html文件放进去，然后增加了url 设置了静态文件夹目录

--- aisinoMx/settings.py
+++ aisinoMx/settings.py
@@ -129,5 +129,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 AUTH_USER_MODEL = 'users.UserProfile'
